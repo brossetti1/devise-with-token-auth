ea52354d9bb45277e4e86615525f7a01bbef6ac0
Dylan Dechant <dylan.dechant@gmail.com>
2015-03-06 21:47:05 -0500

    whoops

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "registrations", :sessions => "sessions"}
-  get '/games/:id', to: 'game#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
